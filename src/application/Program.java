50fa9dd787f63e1b3e3016d6c63fe68226f0d946
fernandoEAD <fernando-danatas@hotmail.com>
2022-01-12 17:09:36 -0300

    Acrescentada mensagem de bom dia

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Bom dia!");
+		
+	}
+}
